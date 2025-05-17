89208d4b00bd0e3e60c3c7d09d1edb5106d4c761
Anmol Rai <itsrai.anmol@gmail.com>
2025-05-18 00:49:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-locust -f locustfile.py --headless -u 10 -r 2 -t 1m --csv=reports/report
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the test
+python -m locust \
+  -f locustfile.py \
+  --headless \
+  -u 10 \
+  -r 2 \
+  -t 1m \
+  --csv=reports/report \
+  --host=https://$JFROG_PLATFORM_ID.jfrog.io
